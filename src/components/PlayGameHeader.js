33dcb581f6b1fc18d0d53b5ee45e4af8d8caff72
Kyle-Close <k.james.close@gmail.com>
2023-06-01 09:22:27 -0700

    added gaps in header

--- src/components/PlayGameHeader.js
+++ src/components/PlayGameHeader.js
@@ -1,18 +1,24 @@
-import React from "react";
+import React from 'react';
 
-import ratchet from '../img/ratchet.webp'
-import kratos from '../img/Kratos.jpg'
-import sackboy from '../img/sackboy.webp'
+import ratchet from '../img/ratchet.webp';
+import kratos from '../img/Kratos.jpg';
+import sackboy from '../img/sackboy.webp';
 
-function PlayGameHeader(){
-    return (
-        <div className="play-game-header">
-            <img src={ratchet} />
-            <img src={kratos} />
-            <img src={sackboy} />
-            <h3>1:32</h3>
-        </div>
-    )
+function PlayGameHeader() {
+	return (
+		<div className='play-game-header'>
+			<div>
+				<img src={ratchet} />
+			</div>
+			<div>
+				<img src={kratos} />
+			</div>
+			<div>
+				<img src={sackboy} />
+			</div>
+			<h3>1:32</h3>
+		</div>
+	);
 }
 
-export default PlayGameHeader
+export default PlayGameHeader;
