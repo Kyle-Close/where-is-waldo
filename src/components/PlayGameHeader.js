a81b606a89f16c3a8842aa85c250d74eaaf7e26b
Kyle Close <k.james.close@gmail.com>
2023-06-01 02:58:17 -0400

    play game header almost styled

--- src/components/PlayGameHeader.js
+++ src/components/PlayGameHeader.js
@@ -1,7 +1,17 @@
 import React from "react";
 
+import ratchet from '../img/ratchet.webp'
+import kratos from '../img/Kratos.jpg'
+import sackboy from '../img/sackboy.webp'
+
 function PlayGameHeader(){
-    return <div className="play-game-header">PlayGameHeader</div>
+    return (
+        <div className="play-game-header">
+            <img src={ratchet} />
+            <img src={kratos} />
+            <img src={sackboy} />
+        </div>
+    )
 }
 
 export default PlayGameHeader
