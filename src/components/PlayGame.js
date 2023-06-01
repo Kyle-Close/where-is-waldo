78e478a2cd66abef6a070f294fc31c107368347e
Kyle Close <k.james.close@gmail.com>
2023-06-01 02:17:15 -0400

    Good spot for both mobile & desktop for play game img

--- src/components/PlayGame.js
+++ src/components/PlayGame.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import PlayGameHeader from './PlayGameHeader';
+
 function PlayGame({ gameImage, isDisabled }) {
 	const className = isDisabled ? 'play-game-disabled' : 'play-game-enabled';
 	return (
 		<div className='play-game-container'>
+			<PlayGameHeader />
 			<img
 				className={className}
 				src={gameImage}
---
+++ src/components/PlayGameHeader.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function PlayGameHeader(){
+    return <div className="play-game-header">PlayGameHeader</div>
+}
+
+export default PlayGameHeader
