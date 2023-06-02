9ebedf348476ab63d2dba35bfacbaab02b0a00d8
Kyle Close <k.james.close@gmail.com>
2023-06-01 22:35:27 -0400

    Added timer to play game header

--- src/components/PlayGameHeader.js
+++ src/components/PlayGameHeader.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { Timestamp } from 'firebase/firestore/lite';
 
 import ratchet from '../img/ratchet.webp';
 import kratos from '../img/Kratos.png';
 import sackboy from '../img/sackboy.webp';
 
 function PlayGameHeader() {
+    const [seconds, setSeconds] = React.useState(0);
+    const [startTime, setStartTime] = React.useState();
+
+    // Get time on first render for firebase
+    React.useEffect(() => {
+        setStartTime(Timestamp.now())
+    }, []);
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds(seconds => seconds + 1);
+    }, 1000);
+
+    // Clear interval on unmount
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+     function handleClick(){
+        console.log(Timestamp.now() - startTime)
+     }
 	return (
 		<div className='play-game-header'>
 			<div>
@@ -17,7 +39,7 @@ function PlayGameHeader() {
 				<img src={sackboy} />
 			</div>
 			<div>
-				<h3>1:32</h3>
+				<h3 onClick={handleClick}>{seconds}</h3>
 			</div>
 		</div>
 	);
