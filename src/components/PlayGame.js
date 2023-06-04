f8873aa4eb72a7b3e81cb5a5369e0cf32d08f247
Kyle-Close <k.james.close@gmail.com>
2023-06-04 10:02:02 -0700

    refactored PlayGame

--- src/components/PlayGame.js
+++ src/components/PlayGame.js
@@ -1,112 +1,18 @@
 import React from 'react';
-import { useRef, useEffect } from 'react';
 
 import PlayGameHeader from './PlayGameHeader';
-import cursorImg from '../img/dotted-square.png';
-import cursorFilled from '../img/dotted-square-filled.png';
-import { func } from 'prop-types';
+import GameBoard from './GameBoard';
 
 function PlayGame({ gameImage, isDisabled }) {
-	const imgRef = useRef(); // To access image properties
-	const [placedCursor, setPlacedCursor] = React.useState(null);
-
-	const className = isDisabled ? 'play-game-disabled' : 'play-game-enabled';
-
-	const myCustomCursor = {
-		cursor: `url(${cursorImg}), auto`,
-	};
-
-	useEffect(() => {
-		if (!placedCursor) return;
-
-		const { x, y } = placedCursor;
-		const originalCoords = getOriginalImageCoords([x, y], imgRef);
-		const selectionBox = calculateBox(originalCoords, 64);
-
-		// For testing
-		const targetBox = {
-			topLeft: [300, 1200],
-			bottomRight: [364, 1264],
-		};
-
-		console.log(isOverlap(selectionBox, targetBox));
-	}, [placedCursor]);
-
-	function handleClick(event) {
-		const coords = getClickCoords(event, imgRef);
-
-		setPlacedCursor({
-			x: coords[0],
-			y: coords[1],
-		});
-	}
-
 	return (
 		<div className='play-game-container'>
 			<PlayGameHeader />
-			<img
-				ref={imgRef}
-				style={myCustomCursor}
-				onClick={handleClick}
-				className={className}
-				src={gameImage}
-				alt='Game Image'
+			<GameBoard
+				gameImage={gameImage}
+				isDisabled={isDisabled}
 			/>
-			{placedCursor && (
-				<img
-					src={cursorFilled}
-					style={{
-						position: 'absolute',
-						left: `${placedCursor.x}px`,
-						top: `${placedCursor.y}px`,
-						zIndex: '2',
-					}}
-				/>
-			)}
 		</div>
 	);
-}
-
-function getClickCoords(event, imgRef) {
-	const rect = imgRef.current.getBoundingClientRect();
-
-	const x = event.clientX - rect.left; //x position within the element.
-	const y = event.clientY - rect.top; //y position within the element.
-
-	return [x, y];
-}
-
-function getOriginalImageCoords(coords, imgRef) {
-	const rect = imgRef.current.getBoundingClientRect();
-	const [x, y] = coords;
-
-	const scaleX = imgRef.current.naturalWidth / rect.width; // scaling factor in x-direction
-	const scaleY = imgRef.current.naturalHeight / rect.height; // scaling factor in y-direction
-
-	const originalX = Math.round(x * scaleX); // x-position in original image size
-	const originalY = Math.round(y * scaleY); // y-position in original image size
-
-	return [originalX, originalY];
-}
-
-function calculateBox(topLeftCoords, size) {
-	return {
-		topLeft: topLeftCoords,
-		bottomRight: [topLeftCoords[0] + size, topLeftCoords[1] + size],
-	};
-}
-
-function isOverlap(selectionBox, targetBox) {
-	if (
-		selectionBox.topLeft[0] <= targetBox.bottomRight[0] &&
-		selectionBox.bottomRight[0] >= targetBox.topLeft[0] &&
-		selectionBox.topLeft[1] <= targetBox.bottomRight[1] &&
-		selectionBox.bottomRight[1] >= targetBox.topLeft[1]
-	) {
-		return true;
-	} else {
-		return false;
-	}
 }
 
 export default PlayGame;
